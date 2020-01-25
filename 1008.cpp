6b745583eb38e6387faa801e6c2110b37320fbd1
Jinsane#5 <zhangjqfriend@126.com>
2020-01-25 22:39:48 +0800

    1008

---
+++ 1008.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int up = 0;
+    int down = 0;
+    int count = n;
+    int last = 0;
+    for (int i=0; i<n; i++){
+        int k;
+        cin >> k;
+        if (k > last){
+            up += (k - last);
+        }
+        else {
+            down += (last - k);
+        }
+        last = k;
+    }
+    
+    int sum = count * 5 + up * 6 + down * 4;
+    cout << sum << endl;
+    return 0;
+}
